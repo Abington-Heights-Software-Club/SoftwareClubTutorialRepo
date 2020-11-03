91464ef632fb21e8bc7f22302636c611769d7ba1
leahlud <leahlud11@outlook.com>
2020-11-03 18:52:31 -0500

    Fixed LeahMove
    fixed code of LeahMove

--- Assets/LeahMove.cs
+++ Assets/LeahMove.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using UnityEngine;
 
-public class LeahMove2 : MonoBehaviour
+public class LeahMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,9 +13,10 @@ public class LeahMove2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("l")) 
+        if(Input.GetKeyDown("l"))
         {
-            transform.position = transform.position = new Vector3(1, 1, 0);
+            transform.position = transform.position + new Vector3(-1, 0);
         }
+        
     }
 }
