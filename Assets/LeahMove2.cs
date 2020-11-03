fb0d24983827a4d29af07a03b37bf3f72ee06faf
leahlud <leahlud11@outlook.com>
2020-11-03 16:31:58 -0500

    Leah Sprite
    added Leah sprite and LeahMove

---
+++ Assets/LeahMove2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class LeahMove2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("l")) 
+        {
+            transform.position = transform.position = new Vector3(1, 1, 0);
+        }
+    }
+}
