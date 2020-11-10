b44cb84eaf9015e930ef3250e84347e138fb48d1
LordTepig <tyler8555@gmail.com>
2020-11-10 16:54:57 -0500

    Added Tyler Physics Move

---
+++ Assets/TylerPhysicMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TylerPhysicMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        movement = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement != 0)
+        {
+            rb.velocity = new Vector2(movement, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+}
