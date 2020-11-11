a180c0e0fdb6903034a8c1ffd2f959f6530351d6
leahlud <leahlud11@outlook.com>
2020-11-11 17:17:33 -0500

    Added Physics to Leah
    added LeahPhysMove component to Leah

---
+++ Assets/LeahPhysMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeahPhysMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float movement;
+    public float moveSpeed = 11;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        movement = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement != 0)
+        {
+            rb.velocity = new Vector2(movement * moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+}
